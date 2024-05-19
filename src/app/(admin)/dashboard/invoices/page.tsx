f00cdf6cbb46046269bb46666ff4838f3f62df80
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-05-19 11:33:24 +0330

    feature : add other pages to dashboard

---
+++ src/app/(admin)/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const InvoicesPage: React.FC = () => {
+	return <div>InvoicesPage</div>;
+};
+
+export default InvoicesPage;
---
+++ src/app/(admin)/dashboard/orders/page.tsx
@@ -0,0 +1,5 @@
+const OrdersPage: React.FC = () => {
+	return <div>OrdersPage</div>;
+};
+
+export default OrdersPage;
