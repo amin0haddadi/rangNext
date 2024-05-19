4adb9f55e7e5079650d43b674a2f6dea63850014
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-05-19 11:36:17 +0330

    feature: implement pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Pagination as MPagination, Stack } from "@mui/material";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface IPaginationProps {
 	totalPages: number;
@@ -10,6 +11,12 @@ const Pagination: React.FC<IPaginationProps> = ({ totalPages }) => {
 	const searchParams = useSearchParams();
 	const pathname = usePathname();
 	const { replace } = useRouter();
+	const [currentPage, setCurrentPage] = useState(1);
+
+	useEffect(() => {
+		const page = searchParams.get("page");
+		setCurrentPage(page ? parseInt(page) : 1);
+	}, [searchParams]);
 
 	const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
 		const params = new URLSearchParams(searchParams);
@@ -21,7 +28,12 @@ const Pagination: React.FC<IPaginationProps> = ({ totalPages }) => {
 
 	return (
 		<Stack direction={"column"} alignItems={"center"} my={3}>
-			<MPagination count={totalPages} shape="rounded" onChange={handleChange} />
+			<MPagination
+				count={totalPages}
+				page={currentPage}
+				shape="rounded"
+				onChange={handleChange}
+			/>
 		</Stack>
 	);
 };
